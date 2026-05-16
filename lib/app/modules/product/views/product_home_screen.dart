cc77e605ccbbcdd417f4ac489bd8e411c0ef666b
shubham <shubhamdhaniyan09@gmail.com>
2026-05-16 18:32:06 +0530

    16 may 2026

---
+++ lib/app/modules/product/views/product_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ProductHomeScreen extends StatefulWidget {
+  const ProductHomeScreen({super.key});
+
+  @override
+  State<ProductHomeScreen> createState() => _ProductHomeScreenState();
+}
+
+class _ProductHomeScreenState extends State<ProductHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
